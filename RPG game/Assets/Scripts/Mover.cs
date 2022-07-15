44aa5f44f612b9e7d4c3c5637d1ecb7127ff7624
Oktay Cem Kutlar <101462838+CemKutlar@users.noreply.github.com>
2022-07-15 18:27:00 +0300

    The first NavMeshAgent implementation.

---
+++ RPG game/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
+
