f84d66ea458b7af0ea71d42de5d2892dcc3630dd
Oktay Cem Kutlar <101462838+CemKutlar@users.noreply.github.com>
2022-07-16 09:33:14 +0300

    FollowCamera done.

---
+++ RPG game/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
